a475c9cae7da2188feb93fe5c6c264044113ef99
cjshearer <cjshearer@live.com>
2024-04-27 11:25:58 -0400

    wip

--- default.nix
+++ default.nix
@@ -4,5 +4,144 @@ let
   pkgs = import nixpkgs { config = { }; overlays = [ ]; };
 in
 {
-  ideamaker = pkgs.libsForQt5.callPackage ./ideamaker.nix { };
+  ideamaker = pkgs.libsForQt5.callPackage ./ideamaker.nix {
+    curl = pkgs.curl.overrideAttrs (old: {
+      configureFlags = old.configureFlags ++ [ "--enable-versioned-symbols" ];
+    });
+    # qtbase = pkgs.libsForQt5.qtbase.overrideAttrs
+    #   (old: {
+    #     configureFlags = pkgs.lib.lists.remove "-verbose" old.configureFlags ++ [
+    #       # "QMAKE_CCX=${pkgs.gcc6.outPath}/bin/g++"
+    #       # "QMAKE_CC=${pkgs.gcc6.outPath}/bin/gcc"
+    #       # "-D_GLIBCXX_USE_CXX11_ABI=1"
+    #       # "-developer-build"
+    #       # "-static"
+    #       # "-platform linux-g++-5"
+    #       # "-DQT_NO_VERSION_TAGGING"
+    #     ];
+
+    #     # qmake doesn't rebuild with c++14 unless we change some other flag, 
+    #     # for some stupid (probably cache) reason
+    #     # configureFlags = pkgs.lib.lists.remove "-verbose" old.configureFlags ++ [ "QMAKE_CXXFLAGS+=-std=c++14" ];
+    #     # configureFlags = [
+    #     #   "-plugindir"
+    #     #   "\${out}/\${qtPluginPrefix}"
+    #     #   "-qmldir"
+    #     #   "\${out}/\${qtQmlPrefix}"
+    #     #   "-docdir"
+    #     #   "\${out}/\${qtDocPrefix}"
+    #     #   # "-verbose"
+    #     #   "-confirm-license"
+    #     #   "-opensource"
+    #     #   "-release"
+    #     #   "-shared"
+    #     #   "-accessibility"
+    #     #   "-optimized-qmake"
+    #     #   "-no-strip"
+    #     #   "-system-proxies"
+    #     #   "-pkg-config"
+    #     #   "-gui"
+    #     #   "-widgets"
+    #     #   "-opengl"
+    #     #   "desktop"
+    #     #   "-icu"
+    #     #   "-L"
+    #     #   "/nix/store/dpwk00dz15y9c0jx2zqa0xrwpjv3nnkl-icu4c-73.2/lib"
+    #     #   "-I"
+    #     #   "/nix/store/bw36c93myddknva88m6icspnx4jhc02m-icu4c-73.2-dev/include"
+    #     #   "-pch"
+    #     #   "-sse2"
+    #     #   "-no-sse3"
+    #     #   "-no-ssse3"
+    #     #   "-no-sse4.1"
+    #     #   "-no-sse4.2"
+    #     #   "-no-avx"
+    #     #   "-no-avx2"
+    #     #   "-no-mips_dsp"
+    #     #   "-no-mips_dspr2"
+    #     #   "-system-zlib"
+    #     #   "-L"
+    #     #   "/nix/store/rrhw8p4hdrbh3pq7p2hlc1z2ixxzcxcb-zlib-1.3.1/lib"
+    #     #   "-I"
+    #     #   "/nix/store/vi5lphqwg9cj7xkaggvw5n0wqsmwdlq5-zlib-1.3.1-dev/include"
+    #     #   "-system-libjpeg"
+    #     #   "-L"
+    #     #   "/nix/store/3xryfb3l0kvxkdf1c8g9x0wcn5i7y0g4-libjpeg-turbo-3.0.2/lib"
+    #     #   "-I"
+    #     #   "/nix/store/z4rjfh54kmj84h524pja9lj9dkl128kz-libjpeg-turbo-3.0.2-dev/include"
+    #     #   "-system-harfbuzz"
+    #     #   "-L"
+    #     #   "/nix/store/cv50gzcn2jli6dn56lhkizi93px89zhj-harfbuzz-8.4.0/lib"
+    #     #   "-I"
+    #     #   "/nix/store/h2a0hvrmfzfxfggyfjgwnl3lmdhynm71-harfbuzz-8.4.0-dev/include"
+    #     #   "-system-pcre"
+    #     #   "-openssl-linked"
+    #     #   "-L"
+    #     #   "/nix/store/9xgpmwqw0881kxa9sl498qfmzy7z2ndn-openssl-3.0.13/lib"
+    #     #   "-I"
+    #     #   "/nix/store/aqaxrq9jyrjwsnsly12b2xkqmakcsawb-openssl-3.0.13-dev/include"
+    #     #   "-system-sqlite"
+    #     #   "-plugin-sql-mysql"
+    #     #   "-plugin-sql-psql"
+    #     #   "-make"
+    #     #   "libs"
+    #     #   "-make"
+    #     #   "tools"
+    #     #   "-nomake"
+    #     #   "examples"
+    #     #   "-nomake"
+    #     #   "tests"
+    #     #   "-rpath"
+    #     #   "-xcb"
+    #     #   "-qpa"
+    #     #   "xcb"
+    #     #   "-L"
+    #     #   "/nix/store/x5wm1j2b9l9najwwpikh9wdykrldnspk-libX11-1.8.7/lib"
+    #     #   "-I"
+    #     #   "/nix/store/x5wm1j2b9l9najwwpikh9wdykrldnspk-libX11-1.8.7/include"
+    #     #   "-L"
+    #     #   "/nix/store/bqjivl23gjxrd9xqlnaj2wmb91lwk644-libXext-1.3.6/lib"
+    #     #   "-I"
+    #     #   "/nix/store/bqjivl23gjxrd9xqlnaj2wmb91lwk644-libXext-1.3.6/include"
+    #     #   "-L"
+    #     #   "/nix/store/zd6s0gyf3amm8rrak8gywn26v1rfv2hf-libXrender-0.9.11/lib"
+    #     #   "-I"
+    #     #   "/nix/store/zd6s0gyf3amm8rrak8gywn26v1rfv2hf-libXrender-0.9.11/include"
+    #     #   "-libinput"
+    #     #   "-cups"
+    #     #   "-dbus-linked"
+    #     #   "-glib"
+    #     #   "-system-libpng"
+    #     #   "-gtk"
+    #     #   "-inotify"
+    #     #   "-L"
+    #     #   "/nix/store/crm5p4wvdfwg75r39784225aqhxi3xpx-cups-2.4.7-lib/lib"
+    #     #   "-I"
+    #     #   "/nix/store/jh4ddz1q5cfmik1cnr0al6fkv5gc9l3p-cups-2.4.7-dev/include"
+    #     #   "-L"
+    #     #   "/nix/store/appf34j78kmm7kabhg5bw9fj2mcnvjj6-mariadb-connector-c-3.3.5/lib"
+    #     #   "-I"
+    #     #   "/nix/store/appf34j78kmm7kabhg5bw9fj2mcnvjj6-mariadb-connector-c-3.3.5/include"
+    #     #   "-translationdir"
+    #     #   "/nix/store/5n30k22k8f451grwcmca3fc99lsi85l5-qttranslations-5.15.12/translations"
+    #     #   "QMAKE_CXXFLAGS+=-std=c++17"
+    #     # ];
+    #   });
+
+
+    # configure flags: -prefix /nix/store/3p1vzx851y02grn5fzmjf63qkqsrgsgq-qtbase-5.15.12 -plugindir \$\(out\)/\$\(qtPluginPrefix\) -qmldir \$\(out\)/\$\(qtQmlPrefix\) -docdir \$\(out\)/\$\(qtDocPrefix\) -verbose -confirm-license -opensource -shared -accessibility -optimized-qmake -no-strip -system-proxies -pkg-config -gui -widgets -opengl desktop -icu -L /nix/store/dpwk00dz15y9c0jx2zqa0xrwpjv3nnkl-icu4c-73.2/lib -I /nix/store/bw36c93myddknva88m6icspnx4jhc02m-icu4c-73.2-dev/include -pch -sse2 -no-sse3 -no-ssse3 -no-sse4.1 -no-sse4.2 -no-avx -no-avx2 -no-mips_dsp -no-mips_dspr2 -system-zlib -L /nix/store/rrhw8p4hdrbh3pq7p2hlc1z2ixxzcxcb-zlib-1.3.1/lib -I /nix/store/vi5lphqwg9cj7xkaggvw5n0wqsmwdlq5-zlib-1.3.1-dev/include -system-libjpeg -L /nix/store/3xryfb3l0kvxkdf1c8g9x0wcn5i7y0g4-libjpeg-turbo-3.0.2/lib -I /nix/store/z4rjfh54kmj84h524pja9lj9dkl128kz-libjpeg-turbo-3.0.2-dev/include -system-harfbuzz -L /nix/store/cv50gzcn2jli6dn56lhkizi93px89zhj-harfbuzz-8.4.0/lib -I /nix/store/h2a0hvrmfzfxfggyfjgwnl3lmdhynm71-harfbuzz-8.4.0-dev/include -system-pcre -openssl-linked -L /nix/store/9xgpmwqw0881kxa9sl498qfmzy7z2ndn-openssl-3.0.13/lib -I /nix/store/aqaxrq9jyrjwsnsly12b2xkqmakcsawb-openssl-3.0.13-dev/include -system-sqlite -plugin-sql-mysql -plugin-sql-psql -make libs -make tools -nomake examples -nomake tests -rpath -xcb -qpa xcb -L /nix/store/x5wm1j2b9l9najwwpikh9wdykrldnspk-libX11-1.8.7/lib -I /nix/store/x5wm1j2b9l9najwwpikh9wdykrldnspk-libX11-1.8.7/include -L /nix/store/bqjivl23gjxrd9xqlnaj2wmb91lwk644-libXext-1.3.6/lib -I /nix/store/bqjivl23gjxrd9xqlnaj2wmb91lwk644-libXext-1.3.6/include -L /nix/store/zd6s0gyf3amm8rrak8gywn26v1rfv2hf-libXrender-0.9.11/lib -I /nix/store/zd6s0gyf3amm8rrak8gywn26v1rfv2hf-libXrender-0.9.11/include -libinput -cups -dbus-linked -glib -system-libpng -gtk -inotify -L /nix/store/crm5p4wvdfwg75r39784225aqhxi3xpx-cups-2.4.7-lib/lib -I /nix/store/jh4ddz1q5cfmik1cnr0al6fkv5gc9l3p-cups-2.4.7-dev/include -L /nix/store/appf34j78kmm7kabhg5bw9fj2mcnvjj6-mariadb-connector-c-3.3.5/lib -I /nix/store/appf34j78kmm7kabhg5bw9fj2mcnvjj6-mariadb-connector-c-3.3.5/include -translationdir /nix/store/5n30k22k8f451grwcmca3fc99lsi85l5-qttranslations-5.15.12/translations QMAKE_CXXFLAGS+=-std=c++14
+    # Creating qmake...
+    # g++ -c -o main.o   -std = c ++ 11
+  };
 }
+
+
+# configureFlags = [ "QMAKE_CXXFLAGS+=-std=c++17" ];
+# configureFlags = old.configureFlags ++ [ "QMAKE_CXXFLAGS+=-std=c++14" ];
+# configureFlags = old.configureFlags ++ [ "-DQT_NO_VERSION_TAGGING" ];
+# configureFlags = old.configureFlags ++ [ "CXXFLAGS=-std=c++17" ];
+# configureFlags = old.configureFlags ++ [ "QMAKE_CXXFLAGS+=-std=c++17" ];
+# configureFlags = old.configureFlags ++ [ "QMAKE_CXXFLAGS=-std=c++17" ];
+# configureFlags = old.configureFlags ++ [ "QMAKE_CXXFLAGS += -std=c++17" ];
+# env.CXXFLAGS = "-std=c++17";
+# env.NIX_CFLAGS_COMPILE = "-std=c++17";
--- ideamaker.nix
+++ ideamaker.nix
@@ -2,41 +2,134 @@
 , autoPatchelfHook
 , curl
 , dpkg
+, e2fsprogs
 , fetchurl
-, gcc
 , lib
-, libGLU
 , libcork
+, libGLU
+, lz4
 , makeDesktopItem
-, qt5
+, nlopt
+, opencascade-occt
+, openssl_1_1
+, openvdb
+  # , qtbase
 , quazip
-, zlib
+, tbb
+, wrapQtAppsHook
 }:
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "ideamaker";
-  version = "4.0.1";
+  version = "5.0.5";
+  buildNumber = "8310";
 
   src = fetchurl {
-    # N.B. Unfortunately ideamaker adds a number after the patch number in
-    # their release scheme which is not referenced anywhere other than in
-    # the download URL. Because of this, I have chosen to not use ${version}
-    # and just handwrite the correct values in the following URL, hopefully
-    # avoiding surprises for the next person that comes to update this
-    # package.
-    url = "https://download.raise3d.com/ideamaker/release/4.0.1/ideaMaker_4.0.1.4802-ubuntu_amd64.deb";
-    sha256 = "sha256-veHEaKmV/PdebyvH6L5vUV4btCTP2SJhN9+qkyhYSOc=";
+    url = "https://download.raise3d.com/${finalAttrs.pname}/release/${finalAttrs.version}/ideaMaker_${finalAttrs.version}.${finalAttrs.buildNumber}-ubuntu_amd64.deb";
+    sha256 = "sha256-qPYfpZfBmiGz4+4OsBTN3Rulb/AjjS4vIuzXLAZChfQ=";
   };
 
-  nativeBuildInputs = [ autoPatchelfHook dpkg qt5.wrapQtAppsHook ];
+  # Fixes runtime error:
+  # no version information available
+  # curl-with-versioned-symbols = curl.overrideAttrs (old: {
+  #   configureFlags = old.configureFlags ++ [ "--enable-versioned-symbols" ];
+  # });
+
+  # myQtbase = qtbase.override {
+  #   withGtk3 = true;
+  # };
+
+  # Fix for sized delete operator (_ZdlPvm) missing in libQt5Core.so:
+  # Without -D_GLIBCXX_USE_CXX11_ABI=1, _ZdlPvm is only defined in libstdc++.so,
+  # causing undefined symbol errors in Qt 5.15.0. This flag enables C++11 ABI,
+  # which includes _ZdlPvm, and ensures it's linked into libQt5Core.so.
+  # 
+  # Want:
+  # objdump -T original/ideamaker/libs/libQt5Core.so.5.15.2 | grep _ZdlPvm
+  # 00000000003cbae0 g    DF .text  0000000000000005  Qt_5        _ZdlPvm
+  #
+  # Got:
+  # objdump -T /nix/store/...-qtbase-5.15.12/lib/libQt5Core.so.5.15.12 | grep _ZdlPvm
+  # 0000000000000000      DF *UND*  0000000000000000 (CXXABI_1.3.9) _ZdlPvm
+  # 
+  # References:
+  # https://stackoverflow.com/questions/53022608/application-crashes-with-symbol-zdlpvm-version-qt-5-not-defined-in-file-libqt
+  # https://community.intel.com/t5/Intel-C-Compiler/What-are-the-flags-so-that-the-sized-delete-operator-ZdlPvm-is/m-p/1355293
+  # https://forum.qt.io/post/656903
+  # https://github.com/Theano/Theano/issues/5141
+  # myQtbase = qtbase.overrideAttrs (old: {
+  #   configureFlags = old.configureFlags ++ [
+  #     # "QMAKE_CXXFLAGS+=-D_GLIBCXX_USE_CXX11_ABI=1"
+  #     # results in:
+  #     # objdump -T /nix/store/12ghsdc4317xjsr5fxjni34jqzsayall-qtbase-5.15.12/lib/libQt5Core.so.5 | grep _ZdlPvm
+  #     # 0000000000000000      DF *UND*  0000000000000000 (CXXABI_1.3.9) _ZdlPvm
+
+  #     # "-D_GLIBCXX_USE_CXX11_ABI=1"
+  #     # results in:
+  #     # objdump -T /nix/store/...-qtbase-5.15.12/lib/libQt5Core.so | grep _ZdlPvm
+  #     # 0000000000000000      DF *UND*  0000000000000000 (CXXABI_1.3.9) _ZdlPvm
+
+  #     # "QMAKE_CXXFLAGS+=-D_GLIBCXX_USE_CXX11_ABI=0"
+  #     # results in:
+  #     # objdump -T /nix/store/c8yrlbsv87hp7qf85z5wgzjs7fj26zwp-qtbase-5.15.12/lib/libQt5Core.so.5.15.12 | grep _ZdlPvm
+  #     # 0000000000000000      DF *UND*  0000000000000000 (CXXABI_1.3.9) _ZdlPvm
+
+  #     # "QMAKE_CXXFLAGS+=-fno-sized-deallocation"
+  #     # results in:
+  #     # objdump -T /nix/store/5d73558bwnxvcb2m8v8ssngzfphhxls1-qtbase-5.15.12/lib/libQt5Core.so.5.15.12 | grep _ZdlPvm
+  #     # <empty output>
+
+  #     # "--enable-versioned-symbols" invalid flag
+  #   ];
+  # });
+
+  # CXXFLAGS = [
+  #   "-std=c++17"
+  # ];
+
+  nativeBuildInputs = [
+    autoPatchelfHook
+    dpkg
+    wrapQtAppsHook
+  ];
   buildInputs = [
     curl
-    gcc.cc.lib
-    libGLU
+    # ^supplies libcurl.so.4, wanted by ideamaker
     libcork
-    qt5.qtbase
-    qt5.qtserialport
+    # ^supplies:
+    # libcork.so.1 wanted by libIMCoreMesh.so.1.0.0
+    libGLU
+    # ^supplies:
+    # libGLU.so.1 wanted by ideamaker and libIMCoreRenderTool.so.1.0.0
+    lz4
+    # ^supplies:
+    # liblz4.so.1 wanted by libblosc.so.1.14.2
+    nlopt
+    # ^supplies:
+    # libnlopt.so.0 wanted by libIMCoreSlice.so.1.0.0
+    opencascade-occt
+    # ^supplies:
+    # libTKLCAF.so.7 wanted by libIMCoreFileFormat.so.1.0.0
+    # libTKMath.so.7 wanted libIMCoreFileFormat.so.1.0.0
+    # libTKMesh.so.7 wanted libIMCoreFileFormat.so.1.0.0
+    # libTKTopAlgo.so.7 wanted libIMCoreFileFormat.so.1.0.0
+    # libTKXCAF.so.7 wanted libIMCoreFileFormat.so.1.0.0
+    # libTKXDESTEP.so.7 wanted libIMCoreFileFormat.so.1.0.0
+    # libTKXDEIGES.so.7 wanted libIMCoreFileFormat.so.1.0.0
+    # libTKXSBase.so.7 wanted libIMCoreFileFormat.so.1.0.0
+    openssl_1_1
+    # ^supplies:
+    # libcrypto.so.1.1, wanted by ideamaker
+    # qt5.full
+    # ^supplies:
+    # libQt5Network.so.5 wanted by ideamaker
+    # libQt5Widgets.so.5 wanted by ideamaker
+    # libQt5Gui.so.5 wanted by ideamaker, libIMCoreFileFormat.so.1.0.0, libIMCoreMesh.so.1.0.0, libIMCoreProfileTool.so.1.0.0, libIMCoreRenderTool.so.1.0.0, libIMCoreSlice.so.1.0.0, libIMCoreUtility.so.1.0.0
+    # libQt5Core.so.5 wanted by ideamaker, libIMCoreFileFormat.so.1.0.0, libpoly2tri.so.1.0.0, libIMCoreMath.so.1.0.0, libIMCoreMesh.so.1.0.0, libquazip.so.1.0.0, libIMCoreProfileTool.so.1.0.0, libIMCoreRenderTool.so.1.0.0, libIMCoreSlice.so.1.0.0, libIMCoreUtility.so.1.0.0, libIMPlugin1.so.1.0.0
     quazip
-    zlib
+    # ^supplies:
+    # libquazip1-qt5.so (originally libquazip.so.1), wanted by libIMCoreFileFormat.so.1.0.0,libIMCoreProfileTool.so.1.0.0, libIMCoreUtility.so.1.0.0
+    tbb
+    # ^supplies: libtbb.so.2 wanted by ideamaker, libIMCoreFileFormat.so.1.0.0,libIMCoreMesh.so.1.0.0, libopenvdb.so.5.0.0, libIMCoreRenderTool.so.1.0.0, libIMCoreSlice.so.1.0.0, libIMCoreUtility.so.1.0.0, libIMPlugin1.so.1.0.0
   ];
 
   unpackPhase = ''
@@ -48,22 +141,46 @@ stdenv.mkDerivation rec {
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/{bin,lib,share/pixmaps}
+    install -D usr/lib/x86_64-linux-gnu/ideamaker/ideamaker $out/bin/${finalAttrs.pname}
 
-    cp usr/lib/x86_64-linux-gnu/ideamaker/ideamaker $out/bin
-    cp usr/lib/x86_64-linux-gnu/ideamaker/libquazip.so.1 $out/lib
-    ln -s "${desktopItem}/share/applications" $out/share/
-    cp usr/share/ideamaker/icons/ideamaker-icon.png $out/share/pixmaps/${pname}.png
+    objcopy --redefine-sym _ZdlPvm@@Qt_5=_ZdlPvm@CXXABI_1.3.9 $out/bin/${finalAttrs.pname}
+
+    # Proprietary libs:
+    mkdir $out/lib
+    cp -a usr/lib/x86_64-linux-gnu/ideamaker/libIM* $out/lib
+    # objcopy --redefine-sym _ZdlPvm@@Qt_5=_ZdlPvm@CXXABI_1.3.9 $out/lib/*
+
+    # cp -a usr/lib/x86_64-linux-gnu/ideamaker/lib{Qt5,icu*}* $out/lib
+    # cp -a usr/lib/x86_64-linux-gnu/ideamaker/plugins $out/lib
+
+    # libIMCoreMath.so.1.0.0 wants libpoly2tri.so.1, but libsForQt5.poly2tri-c only has libpoly2tri-c-0.1.so (is it a drop in replacement?), so we copy it
+    cp -a usr/lib/x86_64-linux-gnu/ideamaker/libpoly2tri.so* $out/lib
+    objcopy --redefine-sym _ZdlPvm@@Qt_5=_ZdlPvm@CXXABI_1.3.9 $out/lib/libpoly2tri.so.1.0.0
+  
+    # libIMCoreUtility.so.1.0.0, libIMCoreProfileTool.so.1.0.0, and
+    # libIMCoreFileFormat.so.1.0.0 wants libquazip.so.1, but libsForQt5.quazip 
+    # only has libquazip1-qt5.so.1.4.0, so we copy it
+    # cp -a usr/lib/x86_64-linux-gnu/ideamaker/libquazip.so* $out/lib
+    patchelf --replace-needed libquazip.so.1 libquazip1-qt5.so $out/lib/*
+
+    # libIMCoreMesh.so.1.0.0 wants libopenvdb.so.5.0, but libsForQt5.openvdb
+    # only has libopenvdb.so.10.0, so we copy it, as well as its transitive  
+    # dependencies:
+    cp -a usr/lib/x86_64-linux-gnu/ideamaker/{libopenvdb,libHalf,libblosc,liblog4cplus,libboost_iostreams,libsnappy}* $out/lib
+    # TODO: check if we can just patchelf --replace-needed
+
+    install -D usr/share/ideamaker/icons/ideamaker-icon.png $out/share/pixmaps/${finalAttrs.pname}.png
+    ln -s ${finalAttrs.desktopItem}/share/applications $out/share/
 
     runHook postInstall
   '';
 
   desktopItem = makeDesktopItem {
-    name = pname;
-    exec = pname;
-    icon = pname;
+    name = finalAttrs.pname;
+    exec = finalAttrs.pname;
+    icon = finalAttrs.pname;
     desktopName = "Ideamaker";
-    genericName = meta.description;
+    genericName = finalAttrs.meta.description;
     categories = [ "Utility" "Viewer" "Engineering" ];
     mimeTypes = [ "application/sla" ];
   };
@@ -74,6 +191,6 @@ stdenv.mkDerivation rec {
     sourceProvenance = with sourceTypes; [ binaryNativeCode ];
     license = licenses.unfree;
     platforms = [ "x86_64-linux" ];
-    maintainers = with maintainers; [ lovesegfault ];
+    maintainers = with maintainers; [ ];
   };
-}
+})
