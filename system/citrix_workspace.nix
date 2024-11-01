4525825a3cf5727ece67da80979058bd805ad1d2
cjshearer <cjshearer@live.com>
2024-10-31 20:47:08 -0400

    flake update

--- system/citrix_workspace.nix
+++ system/citrix_workspace.nix
@@ -7,6 +7,18 @@ in
   options.programs.citrix_workspace.enable = mkEnableOption "citrix_workspace";
 
   config = mkIf cfg.enable {
-    environment.systemPackages = [ pkgs.unstable.citrix_workspace ];
+    # nix-prefetch-url file://$(realpath ~/Downloads/linuxx64-24.8.0.98.tar.gz)
+    environment.systemPackages = [
+      # temporary fix pending: https://github.com/NixOS/nixpkgs/issues/348868
+      (pkgs.unstable.citrix_workspace.override {
+        libvorbis = pkgs.unstable.libvorbis.override {
+          libogg = pkgs.unstable.libogg.overrideAttrs (prevAttrs: {
+            cmakeFlags = (prevAttrs.cmakeFlags or [ ]) ++ [
+              (lib.cmakeBool "BUILD_SHARED_LIBS" true)
+            ];
+          });
+        };
+      })
+    ];
   };
 }
