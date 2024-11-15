f80551c060b3a58eb742b948ce94168b080ebb14
cjshearer <cjshearer@live.com>
2024-11-14 20:31:03 -0500

    tailscale: enable charon as exit-node

--- system/tailscale.nix
+++ system/tailscale.nix
@@ -1,4 +1,14 @@
 { systemConfig, lib, config, pkgs, ... }: lib.mkIf config.services.tailscale.enable {
-  services.tailscale.extraUpFlags = [ "--ssh" "--reset" ];
-  services.tailscale.package = pkgs.tailscale;
+
+  services.tailscale.extraUpFlags =
+    [
+      "--ssh"
+      "--reset"
+    ] ++ builtins.attrNames (
+      lib.filterAttrs
+        (flag: hostnames: builtins.elem systemConfig.hostname hostnames)
+        {
+          "--advertise-exit-node" = [ "charon" ];
+        }
+    );
 }
