9d77bcd2e042dde9fd489b2ee33752443e5a3293
cjshearer <cjshearer@live.com>
2024-12-29 14:47:42 -0500

    tailscale: add notes for NetworkManager workaround

--- system/tailscale.nix
+++ system/tailscale.nix
@@ -17,4 +17,8 @@
     charon = "server";
     sisyphus = "client";
   }.${systemConfig.hostname};
+
+  # Workaround for "Failed to start Network Manager Wait Online." Toggle this on while switching
+  # configurations if you see this failure
+  # systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
 }
