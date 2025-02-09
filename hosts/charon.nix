0f54a2bdfb15f05737668a62aa9c37a651198d5d
cjshearer <cjshearer@live.com>
2025-02-09 14:51:21 -0500

    home-manager: fix charon missing stateVersion

--- hosts/charon.nix
+++ hosts/charon.nix
@@ -33,4 +33,14 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home-manager.users.${systemConfig.username}.home.stateVersion = "24.05";
 }
