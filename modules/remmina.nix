47ad4a888a34a2eb448d00905a919c66ad16e52d
cjshearer <cjshearer@live.com>
2025-02-12 08:25:20 -0500

    remmina: auto launch rdp

--- modules/remmina.nix
+++ modules/remmina.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, config, ... }:
+{ lib, pkgs, systemConfig, config, ... }:
 with lib;
 let
   cfg = config.programs.remmina;
@@ -8,5 +8,32 @@ in
 
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [ remmina ];
+
+    home-manager.users.${systemConfig.username}.systemd.user.services.launch-work-rdp = {
+      Unit = {
+        Description = "Launches work RDP upon detection of ~/Downloads/app.rdp";
+      };
+      Install = {
+        WantedBy = [ "graphical.target" ];
+      };
+      Service = {
+        Type = "simple";
+        ExecStart = pkgs.writeShellScript "launch-work-rdp" ''
+          find ~/Downloads/ -name "app*.rdp" -delete
+          ${pkgs.inotify-tools}/bin/inotifywait -m -e create ~/Downloads | while read path action file; do
+            if [ "$file" != "app.rdp" ]; then
+              continue
+            fi
+            execpath=$(sed -nr 's/shell working directory:s:(.*)/\1/p' "$path/$file")
+            loadbalanceinfo=$(sed -nr 's/loadbalanceinfo:s:(.*)/\1/p' "$path/$file")
+            find ~/Downloads/ -name "app*.rdp" -delete
+            remmina --update-profile ~/.local/share/remmina/work.remmina --set-option execpath="$execpath" --set-option loadbalanceinfo="$loadbalanceinfo"
+            remmina -c ~/.local/share/remmina/work.remmina
+          done
+        '';
+        RestartSec = 10;
+        Restart = "on-failure";
+      };
+    };
   };
 }
