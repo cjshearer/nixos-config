7d75b7183202296bbeb6d07f30969c7aab6a263d
cjshearer <cjshearer@live.com>
2025-02-18 20:22:24 -0500

    fix: disable autin daemon

--- modules/atuin.nix
+++ modules/atuin.nix
@@ -9,7 +9,6 @@ in
   config = mkIf cfg.enable {
     home-manager.users.${systemConfig.username}.programs = {
       atuin = {
-        daemon.enable = true;
         enable = true;
         enableBashIntegration = true;
       };
