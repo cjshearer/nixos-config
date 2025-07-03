92c2bf2324e59e63cbf2fb5ce331b515309a6e12
cjshearer <cjshearer@live.com>
2025-07-03 09:32:49 -0400

    git: auto setup remote

--- modules/git.nix
+++ modules/git.nix
@@ -10,6 +10,7 @@
 
       extraConfig = {
         pull.rebase = true;
+        push.autoSetupRemote = true;
         rebase.autoStash = true;
         # TODO: https://jvns.ca/blog/2024/02/16/popular-git-config-options
       };
