861af212c2f80ba9acd6909287a101b14c56eb29
cjshearer <cjshearer@live.com>
2025-09-13 18:50:22 -0400

    flake: update inputs

--- modules/ytmdesktop.nix
+++ modules/ytmdesktop.nix
@@ -13,37 +13,9 @@ in
 
   config = mkIf cfg.enable {
     environment.systemPackages = [
-      (
-        (pkgs.ytmdesktop.override {
-          # TODO: validate this
-          commandLineArgs = "--password-store=\"gnome-libsecret\"";
-        }).overrideAttrs
-        (finalAttrs: {
-          version = "2.0.10";
-
-          src = pkgs.fetchFromGitHub {
-            owner = "ytmdesktop";
-            repo = "ytmdesktop";
-            tag = "v${finalAttrs.version}";
-            leaveDotGit = true;
-
-            postFetch = ''
-              cd $out
-              git rev-parse HEAD > .COMMIT
-              find -name .git -print0 | xargs -0 rm -rf
-            '';
-
-            hash = "sha256-CA3Vb7Wp4WrsWSVtIwDxnEt1pWYb73WnhyoMVKoqvOE=";
-          };
-
-          yarnOfflineCache = pkgs.yarn-berry.fetchYarnBerryDeps {
-            inherit (finalAttrs) src missingHashes;
-            hash = "sha256-1jlnVY4KWm+w3emMkCkdwUtkqRB9ZymPPGuvgfQolrA=";
-          };
-
-          passthru.updateScript = pkgs.nix-update-script { };
-        })
-      )
+      (pkgs.ytmdesktop.override {
+        commandLineArgs = "--password-store=\"gnome-libsecret\"";
+      })
     ];
 
     # required for last.fm integration
