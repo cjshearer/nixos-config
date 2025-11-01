b3f1e169fa5fc329c6036b11b1d0ba1644d0a225
cjshearer <cjshearer@live.com>
2025-11-01 15:57:53 -0400

    immich: stop serving with tailscale on exit

--- modules/immich.nix
+++ modules/immich.nix
@@ -7,15 +7,14 @@
 # https://github.com/sarveshrulz/nixos_config/blob/main/system/raspberrypi/users/sarvesh/home.nix
 lib.mkIf config.services.immich.enable {
   services.immich.host = "127.0.0.1";
-  systemd.services.tailscale-serve-immich = {
-    description = "Configure Tailscale serving features";
+  systemd.services.tailscaled-serve-immich = {
     after = [ "tailscaled.service" ];
     wants = [ "tailscaled.service" ];
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       Type = "oneshot";
       ExecStart = "${config.services.tailscale.package}/bin/tailscale serve --service=svc:photos ${builtins.toString config.services.immich.port}";
-      ExecStop = "${config.services.tailscale.package}/bin/tailscale serve --reset";
+      ExecStop = "${config.services.tailscale.package}/bin/tailscale serve drain svc:photos";
       RemainAfterExit = true;
     };
   };
