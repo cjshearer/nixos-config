88836752038f18313fce07d7bc04e315da93d35f
cjshearer <cjshearer@live.com>
2025-11-15 14:46:12 -0500

    git: add popular config options

--- modules/git.nix
+++ modules/git.nix
@@ -5,15 +5,19 @@
   config = lib.mkIf config.users.cjshearer.programs.git.enable {
     home-manager.users.cjshearer.programs.git = {
       enable = true;
-
-      userName = "cjshearer";
-      userEmail = "cjshearer@live.com";
-
-      extraConfig = {
+      settings = {
+        # https://jvns.ca/blog/2024/02/16/popular-git-config-options
+        diff.algorithm = "histogram";
+        help.autocorrect = 10;
+        init.defaultBranch = "main";
+        merge.conflictStyle = "zdiff3";
         pull.rebase = true;
         push.autoSetupRemote = true;
         rebase.autoStash = true;
-        # TODO: https://jvns.ca/blog/2024/02/16/popular-git-config-options
+        rere.enabled = true;
+        url."git@github.com:".insteadOf = "https://github.com/";
+        user.email = "cjshearer@live.com";
+        user.name = "cjshearer";
       };
     };
   };
