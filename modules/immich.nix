eea2bd2131328e82b8b0416b1c81701b7e0573cb
cjshearer <cjshearer@live.com>
2025-11-16 16:17:41 -0500

    immich: refine systemd service dependencies
    Adds `requisite` on rclone-mount:@onedrive.service
    to ensure Immich only starts when OneDrive storage
    is mounted.
    
    Adds `wants` on tailscaled-serve-immich.service
    so Tailscale serve is started with Immich, but
    not required.
    
    Set tailscaled-serve-immich to `partOf`
    immich-server and `requires` tailscaled.service to
    ensure tailscale is running before trying to serve
    and to ensure immich-server stops if
    tailscaled-serve-immich stops.

--- modules/immich.nix
+++ modules/immich.nix
@@ -8,33 +8,22 @@
 # https://github.com/simisimis/s/blob/4b39dccbc5bc850572e7eab40aa9b6dcd20efe65/hosts/kouti/proxy.nix#L348-L360
 # state can be removed with:
 # sudo rm -rf /var/lib/{immich,postgresql,redis-immich} /var/cache/immich/
-let
-  mountPoint = config.users.cjshearer.services.rclone.onedrive.mountPoint;
-in
 lib.mkIf config.services.immich.enable {
   services.immich.database.name = "cjshearer";
   services.immich.database.user = "cjshearer";
   services.immich.group = "users";
   services.immich.host = "127.0.0.1";
-  services.immich.mediaLocation = "${mountPoint}/app/immich";
+  services.immich.mediaLocation = "${config.users.cjshearer.services.rclone.onedrive.mountPoint}/app/immich";
   services.immich.user = "cjshearer";
 
   services.tailscale.enable = true;
 
-  systemd.paths."immich-server" = {
-    pathConfig.DirectoryNotEmpty = mountPoint;
-    wantedBy = [ "multi-user.target" ];
-  };
+  systemd.services.immich-server.requisite = [ "rclone-mount\:@onedrive.service" ];
+  systemd.services.immich-server.wants = [ "tailscaled-serve-immich.service" ];
+
   systemd.services.tailscaled-serve-immich = {
-    after = [
-      "tailscaled.service"
-      "immich-server.service"
-    ];
-    wants = [
-      "tailscaled.service"
-      "immich-server.service"
-    ];
-    wantedBy = [ "multi-user.target" ];
+    partOf = [ "immich-server.service" ];
+    requires = [ "tailscaled.service" ];
     serviceConfig = {
       Type = "oneshot";
       ExecStart = "${config.services.tailscale.package}/bin/tailscale serve --service=svc:photos ${builtins.toString config.services.immich.port}";
