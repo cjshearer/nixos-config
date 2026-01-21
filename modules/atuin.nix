6190c63a4a910783fc2192b5908eee2f119d71a2
cjshearer <cjshearer@live.com>
2026-01-20 19:07:32 -0500

    atuin: add incognito function to bashrc

--- modules/atuin.nix
+++ modules/atuin.nix
@@ -7,5 +7,28 @@
       enable = true;
     };
     home-manager.users.cjshearer.programs.bash.enable = true;
+
+    home-manager.users.cjshearer.programs.bash.bashrcExtra = ''
+      incognito () {
+        if [[ $1 = disable ]] || [[ $1 == d ]]
+        then
+          # disable incognito
+          if [[ -n "''${BLE_VERSION-}" ]]; then
+            echo "no idea what ble is (no incognito mode available here)"
+          else
+            precmd_functions+=(_atuin_precmd)
+            preexec_functions+=(_atuin_preexec)
+          fi
+        else
+          # enable incognito
+          if [[ -n "''${BLE_VERSION-}" ]]; then
+            echo "no idea what ble is (no incognito mode available here)"
+          else
+            precmd_functions=("''${precmd_functions[@]/_atuin_precmd}")
+            preexec_functions=("''${preexec_functions[@]/_atuin_preexec}")
+          fi
+        fi
+      }
+    '';
   };
 }
