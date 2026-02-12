2777e4fcee31e3d7f53c3bf805bb48264bb5b5cc
cjshearer <cjshearer@live.com>
2026-02-12 17:36:54 -0500

    pkgs: organize by-name packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,4 @@
 { pkgs, ... }:
-{
-  prepare-nixos-disk = pkgs.callPackage ./prepare-nixos-disk { };
-  ideamaker = pkgs.callPackage ./ideamaker { };
-  google-photos-takeout-helper = pkgs.callPackage ./google-photos-takeout-helper { };
-}
+builtins.mapAttrs (name: _: (pkgs.callPackage (./. + "/by-name/${name}/default.nix") { })) (
+  builtins.readDir ./by-name
+)
