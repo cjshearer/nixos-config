f5028a121f1ff8811f08f06ed6fae691c6acc9f6
cjshearer <cjshearer@live.com>
2026-02-14 14:52:24 -0500

    vscode: eject from managing vscode settings
    VSCode does not want to be managed and it provides
    a settings synchronization mechanism, so I am
    giving up on this.
    
    Furthermore, when accessing vscode remotely, the
    client is responsible for its own settings, which
    does not align with the nix approach, which could
    only set server's settings (in the case that the
    client is not running on nix, such as with WSL).
    
    My previous thoughts on this, in the case of WSL,
    was to attempt symlinking the vscode settings to
    the Windows filesystem, but this would require
    using powershell to create the symlinks, which
    could technically be set up with an activation
    script, but it's just much easier to use vscode's
    built in settings sync.

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -8,6 +8,11 @@
   options.users.cjshearer.programs.vscode.enable = lib.mkEnableOption "vscode";
 
   config = lib.mkIf config.users.cjshearer.programs.vscode.enable {
+    home-manager.users.cjshearer.home.packages = [
+      pkgs.nil
+      pkgs.biome
+    ];
+
     home-manager.users.cjshearer.programs.vscode = {
       enable = true;
       package = (
@@ -15,154 +20,6 @@
           commandLineArgs = "--password-store=\"gnome-libsecret\"";
         }
       );
-
-      profiles.default.enableUpdateCheck = false;
-      profiles.default.enableExtensionUpdateCheck = false;
-      profiles.default.extensions = pkgs.nix4vscode.forVscodeVersion pkgs.vscode.version (
-        [
-          "bierner.markdown-mermaid"
-          "biomejs.biome"
-          "bradlc.vscode-tailwindcss"
-          "budparr.language-hugo-vscode"
-          "dnut.rewrap-revived"
-          "github.copilot-chat"
-          "golang.go"
-          "jnoortheen.nix-ide"
-          "joshbolduc.commitlint"
-          "phil294.git-log--graph"
-          "rust-lang.rust-analyzer"
-          "streetsidesoftware.code-spell-checker"
-          "tamasfe.even-better-toml"
-          "timonwong.shellcheck"
-          "waderyan.gitblame"
-        ]
-        ++ lib.optionals config.programs.direnv.enable [
-          "mkhl.direnv"
-        ]
-        ++ lib.optionals config.programs.openscad.enable [
-          "leathong.openscad-language-support"
-        ]
-      );
-
-      profiles.default.keybindings = [
-        {
-          key = "ctrl+f4";
-          command = "workbench.action.closeActiveEditor";
-        }
-        {
-          key = "ctrl+w";
-          command = "-workbench.action.closeActiveEditor";
-        }
-        {
-          key = "ctrl+k ctrl+alt+s";
-          command = "git.stageSelectedRanges";
-          when = "editorFocus";
-        }
-        {
-          key = "ctrl+k ctrl+n";
-          command = "git.unstageSelectedRanges";
-          when = "editorFocus";
-        }
-        {
-          key = "shift+alt+up";
-          command = "-editor.action.insertCursorAbove";
-          when = "editorTextFocus";
-        }
-        {
-          key = "shift+alt+up";
-          command = "editor.action.copyLinesUpAction";
-          when = "editorTextFocus && !editorReadonly";
-        }
-        {
-          key = "ctrl+shift+alt+up";
-          command = "-editor.action.copyLinesUpAction";
-          when = "editorTextFocus && !editorReadonly";
-        }
-        {
-          key = "shift+alt+down";
-          command = "editor.action.copyLinesDownAction";
-          when = "editorTextFocus && !editorReadonly";
-        }
-        {
-          key = "shift+alt+down";
-          command = "-editor.action.insertCursorBelow";
-          when = "editorTextFocus";
-        }
-        {
-          key = "ctrl+shift+alt+down";
-          command = "-editor.action.copyLinesDownAction";
-          when = "editorTextFocus && !editorReadonly";
-        }
-        {
-          key = "f5";
-          command = "workbench.action.files.revert";
-          when = "editorFocus";
-        }
-        {
-          key = "ctrl+i";
-          command = "-editor.action.triggerSuggest";
-          when = "editorHasCompletionItemProvider && textInputFocus && !editorReadonly && !suggestWidgetVisible";
-        }
-      ];
-
-      profiles.default.userSettings = {
-        "[markdown]" = {
-          "rewrap.autoWrap.enabled" = false;
-        };
-        "[nix]" = {
-          "editor.defaultFormatter" = "jnoortheen.nix-ide";
-        };
-        "[yaml]" = {
-          "editor.autoIndent" = "advanced";
-          "editor.insertSpaces" = true;
-          "editor.tabSize" = 2;
-        };
-        "biome.lsp.bin" = lib.getExe pkgs.biome;
-        "cSpell.diagnosticLevel" = "Hint";
-        "cSpell.diagnosticLevelFlaggedWords" = "Hint";
-        "cSpell.textDecorationColor" = "royalblue";
-        "editor.codeActionsOnSave" = {
-          "source.fixAll" = "explicit";
-        };
-        "editor.formatOnSave" = true;
-        "editor.formatOnSaveMode" = "file";
-        "editor.guides.bracketPairs" = true;
-        "editor.inlineSuggest.enabled" = true;
-        "editor.rulers" = [ 100 ];
-        "editor.tabSize" = 2;
-        "editor.wordWrap" = "bounded";
-        "editor.wordWrapColumn" = 100;
-        "explorer.confirmDelete" = false;
-        "explorer.confirmDragAndDrop" = false;
-        "extensions.autoUpdate" = false;
-        "extensions.ignoreRecommendations" = true;
-        "files.exclude" = {
-          "**/.git" = false;
-        };
-        "git.autofetch" = true;
-        "git.confirmSync" = true;
-        "git.terminalGitEditor" = true;
-        "github.copilot.enable" = {
-          "*" = true;
-          "plaintext" = false;
-          "markdown" = true;
-          "scminput" = false;
-        };
-        "javascript.inlayHints.parameterNames.enabled" = "literals";
-        "javascript.inlayHints.propertyDeclarationTypes.enabled" = true;
-        "javascript.referencesCodeLens.showOnAllFunctions" = true;
-        "javascript.updateImportsOnFileMove.enabled" = "always";
-        "nix.enableLanguageServer" = true;
-        "nix.hiddenLanguageServerErrors" = [
-          "textDocument/documentSymbol"
-        ];
-        "nix.serverPath" = lib.getExe pkgs.nil;
-        "telemetry.telemetryLevel" = "off";
-        "typescript.updateImportsOnFileMove.enabled" = "always";
-        "window.customMenuBarAltFocus" = false;
-        "window.titleBarStyle" = "custom";
-        "window.zoomLevel" = 0.5;
-      };
     };
 
     home-manager.users.cjshearer.services.gnome-keyring.enable = true;
