d01f34f73c04307b5a237c005db56bf427d314b3
cjshearer <cjshearer@live.com>
2026-02-19 15:03:15 +0000

    vscode-sever: make vscode optional

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -5,20 +5,25 @@
   ...
 }:
 {
-
   options.users.cjshearer.programs.vscode.enable = lib.mkEnableOption "vscode";
+  options.users.cjshearer.services.vscode-server.enable = lib.mkEnableOption "vscode-server";
 
-  config = lib.mkIf config.users.cjshearer.programs.vscode.enable {
-    home-manager.users.cjshearer = {
-      home.packages = [
-        pkgs.nil
-        pkgs.biome
-      ];
+  config =
+    (lib.mkIf config.home-manager.users.cjshearer.programs.vscode.enable {
+      home-manager.users.cjshearer = {
+        programs.vscode.enable = true;
 
-      programs.vscode.enable = true;
-
-      services.gnome-keyring.enable = true;
-      services.vscode-server.enable = true;
-    };
-  };
+        services.gnome-keyring.enable = true;
+      };
+      users.cjshearer.services.vscode-server.enable = true;
+    })
+    // (lib.mkIf config.users.cjshearer.services.vscode-server.enable {
+      home-manager.users.cjshearer = {
+        home.packages = [
+          pkgs.nil
+          pkgs.biome
+        ];
+        services.vscode-server.enable = true;
+      };
+    });
 }
