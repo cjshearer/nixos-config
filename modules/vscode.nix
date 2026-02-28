2344733f964a009df06d5b96a0d83732d5c09df8
cjshearer <cjshearer@live.com>
2026-02-28 11:13:52 -0500

    vscode: fix conditional module merge
    Use lib.mkMerge instead of // when combining mkIf
    blocks so programs.vscode.enable and
    services.vscode-server.enable evaluate to true as
    intended.

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -8,8 +8,8 @@
   options.users.cjshearer.programs.vscode.enable = lib.mkEnableOption "vscode";
   options.users.cjshearer.services.vscode-server.enable = lib.mkEnableOption "vscode-server";
 
-  config =
-    (lib.mkIf config.home-manager.users.cjshearer.programs.vscode.enable {
+  config = lib.mkMerge [
+    (lib.mkIf config.users.cjshearer.programs.vscode.enable {
       home-manager.users.cjshearer = {
         programs.vscode.enable = true;
 
@@ -17,7 +17,7 @@
       };
       users.cjshearer.services.vscode-server.enable = true;
     })
-    // (lib.mkIf config.users.cjshearer.services.vscode-server.enable {
+    (lib.mkIf config.users.cjshearer.services.vscode-server.enable {
       home-manager.users.cjshearer = {
         home.packages = [
           pkgs.biome
@@ -26,5 +26,6 @@
         ++ lib.optional config.users.cjshearer.programs.go.enable pkgs.gopls;
         services.vscode-server.enable = true;
       };
-    });
+    })
+  ];
 }
