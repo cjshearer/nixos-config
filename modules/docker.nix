c578769ad9ff7488cd3fb51246caa59ddfce2d9a
cjshearer <cjshearer@live.com>
2026-03-02 14:14:25 +0000

    docker: fix WSL mirrored mode routing

--- modules/docker.nix
+++ modules/docker.nix
@@ -4,4 +4,8 @@ lib.mkIf config.virtualisation.docker.enable {
 
   # https://discourse.nixos.org/t/dockers-firewall-conflicting-with-nixoss-networking/70086/6
   networking.firewall.trustedInterfaces = [ "docker0" ];
+
+  # WSL mirrored mode: do not DNAT packets from WSL2's loopback0
+  # https://github.com/moby/moby/issues/48056#issuecomment-2315995230
+  virtualisation.docker.daemon.settings.userland-proxy = !config.wsl.enable;
 }
--- modules/wsl.nix
+++ modules/wsl.nix
@@ -1,10 +1,18 @@
-{ lib, config, nixos-wsl, ... }: {
+{
+  lib,
+  config,
+  nixos-wsl,
+  ...
+}:
+{
   imports = [ nixos-wsl.nixosModules.default ];
 
-  config = lib.mkIf config.wsl.enable {  
+  # .wslconfig
+  # [wsl2]
+  # networkingMode=Mirrored
+  config = lib.mkIf config.wsl.enable {
     wsl.defaultUser = "cjshearer";
     wsl.interop.register = true;
     wsl.ssh-agent.enable = true;
   };
 }
-
