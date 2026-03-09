8e44e8e3e6f63e6b3891c71c371d7cdf060a0fa9
cjshearer <cjshearer@live.com>
2026-03-09 07:51:05 -0400

    lazygit: init

---
+++ modules/lazygit.nix
@@ -0,0 +1,15 @@
+{
+  config,
+  lib,
+  ...
+}:
+{
+  options.users.cjshearer.programs.lazygit.enable = lib.mkEnableOption "lazygit";
+
+  config = lib.mkIf config.users.cjshearer.programs.lazygit.enable {
+    home-manager.users.cjshearer.programs.lazygit = {
+      enable = true;
+      enableBashIntegration = true;
+    };
+  };
+}
