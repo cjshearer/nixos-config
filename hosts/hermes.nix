fe6e52393444761b1a33ad447908f8e9fae93b98
cjshearer <cjshearer@live.com>
2026-03-16 12:51:12 +0000

    hermes: enable github-copilot-cli

--- hosts/hermes.nix
+++ hosts/hermes.nix
@@ -2,6 +2,7 @@
   nixpkgs.hostPlatform = "x86_64-linux";
 
   home-manager.users.cjshearer.programs.atuin.enable = true;
+  home-manager.users.cjshearer.programs.github-copilot-cli.enable = true;
 
   programs.htop.enable = true;
 
