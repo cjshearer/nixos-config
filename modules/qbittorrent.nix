f8212eff4ef8d183f8d7da66b01b9dd6a4bc5481
cjshearer <cjshearer@live.com>
2026-06-23 17:30:28 -0400

    qbittorrent: run as service

--- modules/qbittorrent.nix
+++ modules/qbittorrent.nix
@@ -1,17 +1,49 @@
 {
   lib,
-  pkgs,
   config,
   ...
 }:
-with lib;
-let
-  cfg = config.programs.qbittorrent;
-in
-{
-  options.programs.qbittorrent.enable = mkEnableOption "qbittorrent";
+lib.mkIf config.services.qbittorrent.enable {
+  services.qbittorrent = {
+    user = "cjshearer";
+    group = "users";
+    profileDir = "/home/cjshearer/.local/share/qBittorrent";
+    serverConfig = {
+      LegalNotice.Accepted = true;
+      Preferences.Downloads.SavePath = "/home/cjshearer/onedrive-uploads";
+      Preferences.Downloads.TempPath = "/tmp/qbittorrent/downloads";
+      Preferences.Downloads.TempPathEnabled = true;
+      Preferences.WebUI.AuthSubnetWhitelist = "100.64.0.0/10";
+      Preferences.WebUI.AuthSubnetWhitelistEnabled = true;
+      Preferences.WebUI.LocalHostAuth = false;
+    };
+  };
 
-  config = mkIf cfg.enable {
-    environment.systemPackages = [ pkgs.qbittorrent ];
+  systemd.services.tailscaled-serve-qbittorrent = {
+    partOf = [ "qbittorrent.service" ];
+    requires = [ "tailscaled.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${config.services.tailscale.package}/bin/tailscale serve --service=svc:torrent ${builtins.toString config.services.qbittorrent.webuiPort}";
+      ExecStop = "${config.services.tailscale.package}/bin/tailscale serve drain svc:torrent";
+      RemainAfterExit = true;
+    };
+  };
+
+  systemd.services.qbittorrent.serviceConfig.PrivateUsers = lib.mkForce false;
+  systemd.services.qbittorrent.serviceConfig.ProtectHome = lib.mkForce false;
+
+  users.cjshearer.services.rclone.operations.qbittorrent = {
+    src = "/home/cjshearer/.local/share/qBittorrent";
+    dst = "onedrive:/app/qBittorrent";
+    enable = true;
+    operation = "bisync";
+  };
+
+  users.cjshearer.services.rclone.operations.onedrive-uploads = {
+    src = "/home/cjshearer/onedrive-uploads";
+    dst = "onedrive:/";
+    enable = true;
+    operation = "copy";
   };
 }
