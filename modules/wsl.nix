57303521c0727436c4c9feefd3acc9e2c23092db
cjshearer <cjshearer@live.com>
2026-06-30 19:31:30 +0000

    wsl: add warm burnout theme to windows terminal

--- modules/wsl.nix
+++ modules/wsl.nix
@@ -47,6 +47,40 @@
       destination = ".wslconfig";
     };
 
+    windows.files.windowsTerminalColorScheme = {
+      source = pkgs.writeText "warm-burnout.json" (
+        builtins.toJSON {
+          profiles.defaults.colorScheme = "Warm Burnout Dark";
+          schemes = [
+            {
+              name = "Warm Burnout Dark";
+              background = "#1A1510";
+              foreground = "#BFBDB6";
+              cursorColor = "#F5C56E";
+              selectionBackground = "#33393A";
+              black = "#23211B";
+              red = "#F06B73";
+              green = "#70BF56";
+              yellow = "#FDB04C";
+              blue = "#4FBFFF";
+              purple = "#D0A1FF";
+              cyan = "#93E2C8";
+              white = "#C7C7C7";
+              brightBlack = "#686868";
+              brightRed = "#F07178";
+              brightGreen = "#AAD94C";
+              brightYellow = "#FFB454";
+              brightBlue = "#59C2FF";
+              brightPurple = "#D2A6FF";
+              brightCyan = "#95E6CB";
+              brightWhite = "#FFFFFF";
+            }
+          ];
+        }
+      );
+      destination = "AppData/Local/Microsoft/Windows Terminal/Fragments/WarmBurnout/warm-burnout.json";
+    };
+
     system.activationScripts.syncWindowsFiles =
       let
         powershell = "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe";
