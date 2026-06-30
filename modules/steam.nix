296696c0fb59b1bb0e7475ce6f27b524574feaf5
cjshearer <cjshearer@live.com>
2026-06-30 17:15:05 +0000

    steam: configure autostart via sharedModules

--- modules/steam.nix
+++ modules/steam.nix
@@ -1,19 +1,21 @@
 {
-  lib,
-  config,
-  ...
-}:
-lib.mkIf config.programs.steam.enable {
-  home-manager.users.cjshearer.home.file.".config/autostart/steam.desktop".text = ''
-    [Desktop Entry]
-    Name=Steam
-    Exec=steam %U -silent
-    Icon=steam
-    Terminal=false
-    Type=Application
-    Categories=Network;FileTransfer;Game;
-    MimeType=x-scheme-handler/steam;x-scheme-handler/steamlink;
-    PrefersNonDefaultGPU=true
-    X-KDE-RunOnDiscreteGpu=true
-  '';
+  home-manager.sharedModules = [
+    (
+      { lib, osConfig, ... }:
+      lib.mkIf osConfig.programs.steam.enable {
+        home.file.".config/autostart/steam.desktop".text = ''
+          [Desktop Entry]
+          Name=Steam
+          Exec=steam %U -silent
+          Icon=steam
+          Terminal=false
+          Type=Application
+          Categories=Network;FileTransfer;Game;
+          MimeType=x-scheme-handler/steam;x-scheme-handler/steamlink;
+          PrefersNonDefaultGPU=true
+          X-KDE-RunOnDiscreteGpu=true
+        '';
+      }
+    )
+  ];
 }
