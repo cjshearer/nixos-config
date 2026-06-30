48a6170672b05ad08876cb74322af9e57a48348b
cjshearer <cjshearer@live.com>
2026-06-30 19:31:30 +0000

    helix: add c-sharp support

--- modules/helix.nix
+++ modules/helix.nix
@@ -14,9 +14,18 @@
             language-server.codebook.command = "${pkgs.codebook}/bin/codebook-lsp";
             language-server.nil.command = "${pkgs.nil}/bin/nil";
             language-server.pylsp.command = "${pkgs.python3Packages.python-lsp-server}/bin/pylsp";
+            language-server.roslyn-language-server.args = [
+              "--autoLoadProjects"
+              "--stdio"
+            ];
+            language-server.roslyn-language-server.command = "${pkgs.roslyn-ls}/bin/Microsoft.CodeAnalysis.LanguageServer";
             language-server.ruff.command = "${pkgs.ruff}/bin/ruff";
 
             language = [
+              {
+                name = "c-sharp";
+                language-servers = [ "roslyn-language-server" ];
+              }
               {
                 name = "markdown";
                 language-servers = [ "codebook" ];
