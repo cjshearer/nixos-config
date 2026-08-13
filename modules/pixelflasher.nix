9f8ffd0980da613d60f2609938b8386c077f094d
cjshearer <cjshearer@live.com>
2026-08-12 20:41:27 -0400

    pixelflasher: move version override to module

--- modules/pixelflasher.nix
+++ modules/pixelflasher.nix
@@ -11,10 +11,26 @@ in
 {
   options.programs.pixelflasher.enable = mkEnableOption "pixelflasher";
 
+  options.programs.pixelflasher.package = mkOption {
+    type = types.package;
+    default = (
+      pkgs.pixelflasher.overrideAttrs (finalAttrs: {
+        version = "9.1.5.0";
+        src = pkgs.fetchFromGitHub {
+          owner = "badabing2005";
+          repo = "PixelFlasher";
+          tag = "v${finalAttrs.version}";
+          hash = "sha256-SAo6od26CULyoxufSpMbkLPm+qx+XNak3irQLep5Ubw=";
+        };
+      })
+    );
+    description = "The package to use for pixelflasher.";
+  };
+
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       android-tools
-      pixelflasher
+      cfg.package
     ];
   };
 }
